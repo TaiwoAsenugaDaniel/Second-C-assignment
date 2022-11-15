8fe4c086906eef67ffa71f2381ec6d4ec12c72fb
TaiwoAsenugaDaniel <117589254+TaiwoAsenugaDaniel@users.noreply.github.com>
2022-11-15 19:11:08 -0100

    added new comments to make it understandable

--- Second-C-Assignment/adamProject222464.c
+++ Second-C-Assignment/adamProject222464.c
@@ -13,17 +13,20 @@ int main(){
     int i = 0;
 
     fscanf(fp, "%i", &number);
-    int list[number];
+    int list[number];//list array
     char test_case[100];
-    int j = 1; //counter initialized to 1
 
+ //code block for while loop   
+int j = 1; //while loop counter initialized to 1
 //while loop to loop through the lines of text provided
 while(fgets(test_case, 100, fp)){
 
     // To remove the number of test cases read initially
     test_case[strcspn(test_case, "\r\n")] = '\0';
-    if(j >=2){
-        int steps =0;
+
+    //if-else condition to make sure we start looping from the second line
+    if(j >= 2){
+        int steps = 0;
         
         //for loop to loop through each character in each string
         for(int count = 0; strlen(test_case) > count; count++){
@@ -35,11 +38,18 @@ while(fgets(test_case, 100, fp)){
                 break;
             }
         }
-        // Append steps value to the array
+        // code block to add steps value to the list array created above
         list[i] = steps;
-        printf("%i\n", steps);
+
+        //print function to print the number of steps taken
+        printf("The number of steps taken in this instance is: %i\n", steps);
+        printf("\n");
+
+        //code to increment i variable in for loop.ie to go to the next character in the string
         i++;
     }
+
+    //code to increment j variable in while loop.ie to go to the next line in the text
     j++;
 }
 
